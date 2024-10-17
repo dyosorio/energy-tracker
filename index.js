8d3fd6734c2fe086b5ff1e62a5ebfed60ba42c10
Danny Osorio <dannyosorio@dannys-mbp.home>
2024-10-17 22:28:40 +0200

    Set up Express Server

--- index.js
+++ index.js
@@ -9,9 +9,20 @@ const app = express();
 const upload = multer({ dest: 'uploads/ '});
 
 // Basic route for testing the server
-app.get();
+app.get('/', (req, res) => {
+    res.send('File upload service is running!');
+});
 
 // File upload route
-app.post();
+app.post('/upload', upload.single('file'), (req, res) => {
+    //After uploading the file, it will be available in req.file
+    console.log(req.file);
+
+    res.send('File uploaded successfully');
+});
 
 // Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log('Server is running on port ${PORT}');
+});
