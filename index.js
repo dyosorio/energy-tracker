d5ad421d91ba53b0aef05a5faba83586c3627475
Danny Osorio <dannyosorio@dannys-mbp.home>
2024-10-18 00:45:16 +0200

    Added environment variables file

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const multer = require('multer');
+const csv = require('csv-parser');
+const fs = require('fs');
 const path = require('path');
+const { Pool } = require('pg');
 
 // Initialize Express app
 const app = express();
+
+// PostgreSQL connection setup
+const pool = new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
+});
 
 // Set up a storage location for uploaded files
 const upload = multer({ dest: 'uploads/ '});
@@ -13,7 +25,7 @@ app.get('/', (req, res) => {
     res.send('File upload service is running!');
 });
 
-// File upload route
+// CSV parsing and inserting into PostgreSQL
 app.post('/upload', upload.single('file'), (req, res) => {
     //After uploading the file, it will be available in req.file
     console.log(req.file);
